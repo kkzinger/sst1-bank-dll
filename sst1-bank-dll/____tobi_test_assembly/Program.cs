6cc2bc4f7ba2096244148c341dcedaafbabd0834
Tobias Mayer <tobias.mayer@neumarkt.at>
2016-10-20 20:33:46 +0200

    added project for own testing

---
+++ sst1-bank-dll/____tobi_test_assembly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;     // DLL support
+
+namespace ____tobi_test_assembly
+{
+    class Program
+    {
+        [DllImport("bank_customers.dll")]
+        public static extern int Create
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
