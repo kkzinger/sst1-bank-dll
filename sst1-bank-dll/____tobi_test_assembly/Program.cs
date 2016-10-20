ed8a94826b4e3efae720e8b3683dda49305da5bf
Tobias Mayer <tobias.mayer@neumarkt.at>
2016-10-20 21:31:59 +0200

    casted char* to StringBuilder() with success, typedefs are making problems

--- sst1-bank-dll/____tobi_test_assembly/Program.cs
+++ sst1-bank-dll/____tobi_test_assembly/Program.cs
@@ -4,17 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;     // DLL support
-
 namespace ____tobi_test_assembly
 {
     class Program
     {
-        [DllImport("../../../Release/bank_currency.dll")]
-        public static extern int bar();
+        //[DllImport("../../../Release/bank_currency.dll")]
+        //public static extern int bar();
+
+        [DllImport("../../../Release/bank_entitycomponent.dll")]
+        public static extern int _initEntity();
+
+        [DllImport("../../../Release/bank_customers.dll")]
+        public static extern int Create(StringBuilder FirstName, StringBuilder LastName, StringBuilder Street, StringBuilder StreetNr, StringBuilder City, StringBuilder PostalCode, StringBuilder Country);
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine(bar());
+            Console.WriteLine(_initEntity());
+
+            
+
+            StringBuilder FirstName = new StringBuilder("Tobias");
+            StringBuilder LastName = new StringBuilder("Mayer");
+            StringBuilder Street = new StringBuilder("Salzburger Str.");
+            StringBuilder StreetNr = new StringBuilder("17a");
+            StringBuilder City = new StringBuilder("Neumarkt");
+            StringBuilder PostalCode = new StringBuilder("5202");
+            StringBuilder Country = new StringBuilder("AUSTRIA");
+
+            Console.WriteLine(Create(FirstName, LastName, Street, StreetNr, City, PostalCode, Country));
         }
     }
 }
