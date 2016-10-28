747b8bf0d192b4c35ae4b0d34eece3f743a1aa0c
Tobias Mayer <tobias.mayer@neumarkt.at>
2016-10-28 21:00:24 +0200

    solved the null_ptr problem of the firstname or the struct :)

--- sst1-bank-dll/____tobi_test_assembly/Program.cs
+++ sst1-bank-dll/____tobi_test_assembly/Program.cs
@@ -12,14 +12,21 @@ namespace ____tobi_test_assembly
     {
 
         public uint CID;
-        [MarshalAs(UnmanagedType.LPStr)] public String FirstName;
-        [MarshalAs(UnmanagedType.LPStr)] public String LastName;
-        [MarshalAs(UnmanagedType.LPStr)] public String Street;
-        [MarshalAs(UnmanagedType.LPStr)] public String StreetNr;
-        [MarshalAs(UnmanagedType.LPStr)] public String City;
-        [MarshalAs(UnmanagedType.LPStr)] public String PostalCode;
-        [MarshalAs(UnmanagedType.LPStr)] public String Country;
-         public Byte Active;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst =20)]
+        public string FirstName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string LastName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string Street;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string StreetNr;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string City;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string PostalCode;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string Country;
+         public byte Active;
 
 
     }
@@ -28,8 +35,9 @@ namespace ____tobi_test_assembly
     {
         //[DllImport("../../../Release/bank_currency.dll")]
         //public static extern int bar();
+      
 
-        CUSTOMER C = new CUSTOMER();
+
 
         //[DllImport("../../../Release/bank_entitycomponent.dll")]
         [DllImport("../../../Debug/bank_entitycomponent.dll")]
@@ -40,27 +48,33 @@ namespace ____tobi_test_assembly
         public static extern int Create( String FirstName, String LastName, String Street, String StreetNr, String City, String PostalCode, String Country);
         //     [DllImport("../../../Release/bank_customers.dll")]
       
-        [DllImport("../../../Debug/bank_customers.dll", CallingConvention = CallingConvention.Cdecl/*, CharSet =CharSet.Ansi*/)]
-        internal static extern int Read(uint CID, [In, Out]  ref CUSTOMER C);
+        [DllImport("../../../Debug/bank_customers.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern int Read(uint CID, IntPtr  C);
 
         static void Main(String[] args)
         {
-  
-            Console.WriteLine(_initEntity());
-            CUSTOMER C = new CUSTOMER();
 
-            String  FirstName =   "Tobias";
-            String  LastName =   "Mayer";
-            String  Street =   "Salzburger Str.";
-            String  StreetNr =   "17a";
-            String  City =   "Neumarkt";
-            String  PostalCode =   "5202";
-            String  Country =   "AUSTRIA";
+            //4+7*20+1 = 145
+           IntPtr C = Marshal.AllocHGlobal(145);
+
+            Console.WriteLine(_initEntity());
+    
+
+            string FirstName =   "Tobias";
+            string LastName =   "Mayer";
+            string Street =   "Salzburger Str.";
+            string StreetNr =   "17a";
+            string City =   "Neumarkt";
+            string PostalCode =   "5202";
+            string Country =   "AUSTRIA";
 
             Console.WriteLine(Create(FirstName, LastName, Street, StreetNr, City, PostalCode, Country));
-            Console.WriteLine(Read(1,   ref C));
-            Console.WriteLine(C.CID);
-            Console.WriteLine(C.FirstName);
+            Console.WriteLine(Read(1,  C));
+
+            CUSTOMER C_instance = (CUSTOMER)Marshal.PtrToStructure(C, typeof(CUSTOMER));
+
+            Console.WriteLine(C_instance.CID);
+            Console.WriteLine(C_instance.FirstName);
            
         }
     }
