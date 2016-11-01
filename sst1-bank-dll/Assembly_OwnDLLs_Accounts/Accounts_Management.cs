7df1497a029ef05f81e8d77aa7ae22dd6ea2c38a
Gerold Katzinger <gerold@katzinger.info>
2016-11-02 00:40:48 +0100

    commented testcode in account assembly

--- sst1-bank-dll/Assembly_OwnDLLs_Accounts/Accounts_Management.cs
+++ sst1-bank-dll/Assembly_OwnDLLs_Accounts/Accounts_Management.cs
@@ -245,115 +245,115 @@ namespace Assembly_OwnDLLs_Accounts
 
         static void Main(string[] args)
         {
-            Console.WriteLine(_initEntity());
+            //Console.WriteLine(_initEntity());
 
-            int AID;
-            int CID = 3;
+            //int AID;
+            //int CID = 3;
 
-            uint[] dep = new uint[20] { 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            uint[] readDeps = new uint[20];
-            uint[] addDeps = new uint[] { 10, 11, 12, 13 };
-            uint[] removeDeps = new uint[] { 3, 12 };
-            uint[] AIDlist = new uint[100];
-            account_t type = account_t.SAVING;
-            currency_t curr = currency_t.EUR;
-            float bal = 25.80F;
+            //uint[] dep = new uint[20] { 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //uint[] readDeps = new uint[20];
+            //uint[] addDeps = new uint[] { 10, 11, 12, 13 };
+            //uint[] removeDeps = new uint[] { 3, 12 };
+            //uint[] AIDlist = new uint[100];
+            //account_t type = account_t.SAVING;
+            //currency_t curr = currency_t.EUR;
+            //float bal = 25.80F;
 
-            AID = openAccount(dep, type, curr, bal);
-            if (AID > 0) Console.WriteLine("Account created AID-> {0}", AID);
+            //AID = openAccount(dep, type, curr, bal);
+            //if (AID > 0) Console.WriteLine("Account created AID-> {0}", AID);
 
-            AID = openAccount(dep, type, curr, bal);
-            if (AID > 0) Console.WriteLine("Account created AID-> {0}", AID);
+            //AID = openAccount(dep, type, curr, bal);
+            //if (AID > 0) Console.WriteLine("Account created AID-> {0}", AID);
 
-            AID = openAccount(dep, type, curr, bal);
-            if (AID > 0) Console.WriteLine("Account created AID-> {0}", AID);
+            //AID = openAccount(dep, type, curr, bal);
+            //if (AID > 0) Console.WriteLine("Account created AID-> {0}", AID);
 
-            //Read CIDs that are Depositors off Account and write to console
-            if (getAccountDepositors(AID, readDeps) == 0)
-            {
-                Console.WriteLine("Following Customers are Depositors of Account {0}", AID);
-                for (uint i = 0; i < readDeps.Length; i++)
-                {
-                    //when a zero in Depositor Array is reached no further CIDs will be found.
-                    if (readDeps[i] == 0)
-                    {
-                        Console.Write("\n");
-                        break;
-                    }
+            ////Read CIDs that are Depositors off Account and write to console
+            //if (getAccountDepositors(AID, readDeps) == 0)
+            //{
+            //    Console.WriteLine("Following Customers are Depositors of Account {0}", AID);
+            //    for (uint i = 0; i < readDeps.Length; i++)
+            //    {
+            //        //when a zero in Depositor Array is reached no further CIDs will be found.
+            //        if (readDeps[i] == 0)
+            //        {
+            //            Console.Write("\n");
+            //            break;
+            //        }
 
-                    Console.Write("{0} ", readDeps[i]);
-                }
-            }
+            //        Console.Write("{0} ", readDeps[i]);
+            //    }
+            //}
 
-            Console.WriteLine("Add Customers as Depositors");
-            Console.WriteLine("ergebnis add: {0}", addAccountDepositors(AID, addDeps));
+            //Console.WriteLine("Add Customers as Depositors");
+            //Console.WriteLine("ergebnis add: {0}", addAccountDepositors(AID, addDeps));
 
-            //Read CIDs that are Depositors off Account and write to console
-            if (getAccountDepositors(AID, readDeps) == 0)
-            {
-                Console.WriteLine("Following Customers are Depositors of Account {0}", AID);
-                for (uint i = 0; i < readDeps.Length; i++)
-                {
-                    //when a zero in Depositor Array is reached no further CIDs will be found.
-                    if (readDeps[i] == 0)
-                    {
-                        Console.Write("\n");
-                        break;
-                    }
+            ////Read CIDs that are Depositors off Account and write to console
+            //if (getAccountDepositors(AID, readDeps) == 0)
+            //{
+            //    Console.WriteLine("Following Customers are Depositors of Account {0}", AID);
+            //    for (uint i = 0; i < readDeps.Length; i++)
+            //    {
+            //        //when a zero in Depositor Array is reached no further CIDs will be found.
+            //        if (readDeps[i] == 0)
+            //        {
+            //            Console.Write("\n");
+            //            break;
+            //        }
 
-                    Console.Write("{0} ", readDeps[i]);
-                }
-            }
+            //        Console.Write("{0} ", readDeps[i]);
+            //    }
+            //}
 
-            Console.WriteLine("Remove Customers as Depositors");
-            removeAccountDepositors(AID, removeDeps);
+            //Console.WriteLine("Remove Customers as Depositors");
+            //removeAccountDepositors(AID, removeDeps);
 
-            //Read CIDs that are Depositors off Account and write to console
-            if (getAccountDepositors(AID, readDeps) == 0)
-            {
-                Console.WriteLine("Following Customers are Depositors of Account {0}", AID);
-                for (uint i = 0; i < readDeps.Length; i++)
-                {
-                    //when a zero in Depositor Array is reached no further CIDs will be found.
-                    if (readDeps[i] == 0)
-                    {
-                        Console.Write("\n");
-                        break;
-                    }
+            ////Read CIDs that are Depositors off Account and write to console
+            //if (getAccountDepositors(AID, readDeps) == 0)
+            //{
+            //    Console.WriteLine("Following Customers are Depositors of Account {0}", AID);
+            //    for (uint i = 0; i < readDeps.Length; i++)
+            //    {
+            //        //when a zero in Depositor Array is reached no further CIDs will be found.
+            //        if (readDeps[i] == 0)
+            //        {
+            //            Console.Write("\n");
+            //            break;
+            //        }
 
-                    Console.Write("{0} ", readDeps[i]);
-                }
-            }
+            //        Console.Write("{0} ", readDeps[i]);
+            //    }
+            //}
 
-            Console.WriteLine("Search for all Accounts where Customer {0} is Depositor", CID);
-            getAIDsbyCID(CID, AIDlist);
+            //Console.WriteLine("Search for all Accounts where Customer {0} is Depositor", CID);
+            //getAIDsbyCID(CID, AIDlist);
 
-            for (uint i = 0; i < AIDlist.Length; i++)
-            {
-                if (AIDlist[i] == 0)
-                {
-                    Console.Write("\n");
-                    break;
-                }
+            //for (uint i = 0; i < AIDlist.Length; i++)
+            //{
+            //    if (AIDlist[i] == 0)
+            //    {
+            //        Console.Write("\n");
+            //        break;
+            //    }
 
-                Console.Write("{0} ", AIDlist[i]);
+            //    Console.Write("{0} ", AIDlist[i]);
 
-            }
+            //}
 
-            Console.WriteLine("Is Account {1} Unfrozen? -- {0}", isAccountUnfrozen(AID), AID);
-            freezeAccount(AID);
-            Console.WriteLine("Is Account {1} Unfrozen ? -- {0}", isAccountUnfrozen(AID), AID);
-            unfreezeAccount(AID);
-            Console.WriteLine("Is Account {1} Unfrozen ? -- {0}", isAccountUnfrozen(AID), AID);
+            //Console.WriteLine("Is Account {1} Unfrozen? -- {0}", isAccountUnfrozen(AID), AID);
+            //freezeAccount(AID);
+            //Console.WriteLine("Is Account {1} Unfrozen ? -- {0}", isAccountUnfrozen(AID), AID);
+            //unfreezeAccount(AID);
+            //Console.WriteLine("Is Account {1} Unfrozen ? -- {0}", isAccountUnfrozen(AID), AID);
 
-            Console.WriteLine("Is Account {1} Open ? -- {0}", isAccountOpen(AID), AID);
-            closeAccount(AID);
-            Console.WriteLine("Is Account {1} Open ? -- {0}", isAccountOpen(AID), AID);
+            //Console.WriteLine("Is Account {1} Open ? -- {0}", isAccountOpen(AID), AID);
+            //closeAccount(AID);
+            //Console.WriteLine("Is Account {1} Open ? -- {0}", isAccountOpen(AID), AID);
 
-            account_t readType = account_t.CREDIT;
-            Console.WriteLine(readType);
-            Console.WriteLine(getAccountType(AID, ref readType));
-            Console.WriteLine(readType);
+            //account_t readType = account_t.CREDIT;
+            //Console.WriteLine(readType);
+            //Console.WriteLine(getAccountType(AID, ref readType));
+            //Console.WriteLine(readType);
         }
     }
 }
