db04e4a32eb17ae9b2a7217c363f5a6230a732d1
Gerold Katzinger <gerold@katzinger.info>
2016-11-01 21:06:53 +0100

    add testassembly

---
+++ sst1-bank-dll/test_Assembly_OwnDLLs_AccountActions/test_AccountActions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Assembly_OwnDLLs_Accounts;
+using Assembly_OwnDLLs_Customers;
+
+
+namespace test_Assembly_OwnDLLs_AccountActions
+{
+    class test_AccountActions
+    {
+        static void Main(string[] args)
+        {
+           
+            
+
+        }
+    }
+}
