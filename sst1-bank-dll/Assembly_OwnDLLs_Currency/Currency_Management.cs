275a92af20e00e49a0eea9410071a431da04f2ee
Gerold Katzinger <gerold@katzinger.info>
2016-11-11 00:28:51 +0100

    add  currency assembly

---
+++ sst1-bank-dll/Assembly_OwnDLLs_Currency/Currency_Management.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_OwnDLLs_Currency
+{
+    class Currency_Management
+    {
+
+        public static int addCurrency(string currID, float exchangeRateToEUR)
+        {
+
+
+            return 0;
+        }
+
+        public static int changeCurrencyExchangeRate(string currID, float exchangeRateToEUR)
+        {
+
+
+            return 0;
+        }
+
+        public static int getCurrencyExchangeRate(string currID, ref float exchangeRate)
+        {
+
+            exchangeRate = 1.4242f;
+
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
